ef5ea77a6aff551c02faeaa83e3c2af514d01564
Jens gerwins <Jens.gerwins@gmail.com>
2023-05-19 17:29:05 +0200

    Aktuelle Date

--- age-CalculatorApp/script.js
+++ age-CalculatorApp/script.js
@@ -2,13 +2,17 @@ const inputDay = document.querySelector("#input_day");
 const inputmonth = document.querySelector("#input_month");
 const inputyears = document.querySelector("#input_year");
 const btn = document.querySelector("#btn");
+let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-let years = 0;
-let month = 0;
-let days = 0;
 const actualDate = new Date();
+let day = actualDate.getDate();
+let months = 1 + actualDate.getMonth();
+let year = actualDate.getFullYear();
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", rechnung());
+
+
+function rechnung() {
     days = inputDay.value;
     daysList = document.querySelector("#span_days");
     daysList.innerText = days;
@@ -18,7 +22,31 @@ btn.addEventListener("click", () => {
     years = inputyears.value;
     yearsList = document.querySelector("#span_years");
     yearsList.innerText = years;
+    if (days > day) {
+        days = day + months[month - 1];
+        month = month - 1;
+    }
+    if (month > months) {
+        months = month + 12;
+        year = year - 1;
+
+    }
+    const d = day - days;
+    const m = months - month;
+    const y = year - years;
+    daysList.innerText = d;
+    monthList.innerText = m;
+    yearsList.innerText = y;
+
+}
 
 
+function validDate() {
+    const inputs = document.querySelectorAll("input");
+    let validator = true;
+    inputs.forEach((item) => {
+        const parent = item.parentElement;
 
-})
+    })
+
+}
