7062f419f807290a3ec19036571c5af75e4a8f20
Jens gerwins <Jens.gerwins@gmail.com>
2023-05-19 19:19:12 +0200

    Fehler verbessert

--- age-CalculatorApp/script.js
+++ age-CalculatorApp/script.js
@@ -1,39 +1,38 @@
 const inputDay = document.querySelector("#input_day");
 const inputmonth = document.querySelector("#input_month");
 const inputyears = document.querySelector("#input_year");
+
+const daysList = document.querySelector("#span_days");
+const monthList = document.querySelector("#span_months");
+const yearsList = document.querySelector("#span_years");
+
 const btn = document.querySelector("#btn");
-let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+
+let months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 const actualDate = new Date();
 let day = actualDate.getDate();
-let months = 1 + actualDate.getMonth();
+let month = 1 + actualDate.getMonth();
 let year = actualDate.getFullYear();
 
-btn.addEventListener("click", rechnung());
+btn.addEventListener("click", rechnung);
 
 
 function rechnung() {
-    days = inputDay.value;
-    daysList = document.querySelector("#span_days");
-    daysList.innerText = days;
-    month = inputmonth.value;
-    monthList = document.querySelector("#span_months");
-    monthList.innerText = month;
-    years = inputyears.value;
-    yearsList = document.querySelector("#span_years");
-    yearsList.innerText = years;
-    if (days > day) {
-        days = day + months[month - 1];
+
+    if (inputDay.value > day) {
+        day = day + months[month - 1];
         month = month - 1;
     }
-    if (month > months) {
-        months = month + 12;
+    if (inputmonth.value > month) {
+        month = month + 12;
         year = year - 1;
 
     }
-    const d = day - days;
-    const m = months - month;
-    const y = year - years;
+    const d = day - inputDay.value;
+    const m = month - inputmonth.value;
+    const y = year - inputyears.value;
     daysList.innerText = d;
     monthList.innerText = m;
     yearsList.innerText = y;
