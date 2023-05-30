9f5b0defe6b96eab061597b76416d8fcd03141e0
Jens gerwins <Jens.gerwins@gmail.com>
2023-05-30 16:39:47 +0200

    Hello

--- vue-cli-test/vuetestcli/my-first-project/src/App.vue
+++ vue-cli-test/vuetestcli/my-first-project/src/App.vue
@@ -1,15 +1,17 @@
 <template>
+  <MyComponent />
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-
+import MyComponent from "@/components/MyFirstComponent.vue";
 export default {
   name: "App",
   components: {
     HelloWorld,
+    MyComponent,
   },
 };
 </script>
