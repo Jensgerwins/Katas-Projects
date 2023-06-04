dc3ed942c6bce9945514ab0bd822b242ea1b7f86
Jens gerwins <Jens.gerwins@gmail.com>
2023-06-04 15:52:20 +0200

    first

--- vue-cli-test/vuetestcli/my-first-project/src/components/MyFirstComponent.vue
+++ vue-cli-test/vuetestcli/my-first-project/src/components/MyFirstComponent.vue
@@ -1,7 +1,38 @@
-<template>Hallo Welt</template>
-
+<template>
+  <h1 :class="cssTypeClass">{{ headline }}</h1>
+</template>
 <script>
 export default {
   name: "MyComponent",
+  props: {
+    headline: String,
+    type: {
+      type: String,
+      default: "green",
+    },
+  },
+  computed: {
+    cssTypeClass() {
+      return `text-${this.type}`;
+    },
+  },
 };
 </script>
+
+<style scoped>
+.text-green {
+  color: green;
+}
+.text-red {
+  color: red;
+}
+.text-blue {
+  color: blue;
+}
+.text-purple {
+  color: purple;
+}
+.text-hotpink {
+  color: hotpink;
+}
+</style>
