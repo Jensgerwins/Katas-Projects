149d185b621d1783e3e69be961e15b9e7c31a4eb
Jens gerwins <Jens.gerwins@gmail.com>
2023-06-12 20:31:19 +0200

    a

---
+++ tcalculator/script.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+const add = function (zahl1, zahl2) {
+    return zahl1 + zahl2;
+}
+
+const subt = function (zahl1, zahl2) {
+    return zahl1 - zahl2;
+}
+
+const multi = function (zahl1, zahl2) {
+    return zahl1 * zahl2;
+}
+
+const divdi = function (zahl1, zahl2) {
+    return zahl1 / zahl2;
+}
