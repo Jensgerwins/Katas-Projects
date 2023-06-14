f93e04ee477451f48753cf211914da067ce22710
Jens gerwins <Jens.gerwins@gmail.com>
2023-06-14 19:46:53 +0200

    rechnung

---
+++ fetch/E-commerce product page/script.js
@@ -0,0 +1,50 @@
+//.waren - korb
+//warenkorb - header
+"use strict";
+const warenSvg1 = document.querySelector("#warenkorb-header");
+const warenCheck = document.querySelector("#warencheckout");
+const plusbtn = document.querySelector("#product-plus");
+const minbtn = document.querySelector("#product-minus");
+const btn = document.querySelector("#product-button");
+const anzahldisplay = document.querySelector("#product-anzahl");
+const textwarenkorb = document.querySelector("#Empty-warencorp");
+const newprice = document.querySelector("#new-price");
+let anzahl = 0;
+let price = 0;
+let aktuelerpreis = 125.00;
+
+
+
+
+
+warenSvg1.addEventListener("click", warenkorbCheck);
+
+plusbtn.addEventListener("click", () => {
+    anzahl = anzahl + 1;
+    anzahldisplay.innerHTML = anzahl;
+})
+
+minbtn.addEventListener("click", () => {
+    if (anzahl < 1) {
+
+        return anzahldisplay.innerHTML = 0;
+    } else
+
+        anzahl = anzahl - 1;
+    anzahldisplay.innerHTML = anzahl;
+})
+
+btn.addEventListener("click", () => {
+    const summe = aktuelerpreis * anzahl
+    return summe;
+
+})
+
+
+
+function warenkorbCheck() {
+    if (warenCheck.style.display == "block") {
+        return warenCheck.style.display = "none";
+    }
+    return warenCheck.style.display = "block";
+}
